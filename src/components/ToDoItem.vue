aa3acd86a129259300f2be06e98b2bc4ad297227
idu-who <104561260+idu-who@users.noreply.github.com>
2024-03-23 07:21:13 +0400

    todoitem with unique id
    Signed-off-by: idu-who <104561260+idu-who@users.noreply.github.com>

--- src/components/ToDoItem.vue
+++ src/components/ToDoItem.vue
@@ -1,15 +1,22 @@
 <template>
     <div>
-        <input type="checkbox" id="todo-item" :checked="done" />
-        <label for="todo-item">{{ label }}</label>
+        <input type="checkbox" :id="id" :checked="done" />
+        <label :for="id">{{ label }} {{ id }}</label>
     </div>
 </template>
 
 <script>
+import uniqueId from "lodash.uniqueid";
+
 export default {
     props: {
         label: { required: true, type: String },
         done: { default: false, type: Boolean },
+    },
+    data() {
+        return {
+            id: uniqueId("todo-"),
+        };
     },
 };
 </script>
