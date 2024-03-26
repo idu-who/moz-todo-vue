22ed12a2d29a9503a31a5ecb16720137f703160c
idu-who <104561260+idu-who@users.noreply.github.com>
2024-03-26 15:02:46 +0400

    created ToDoForm
    Signed-off-by: idu-who <104561260+idu-who@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="app">
         <h1>To-Do List</h1>
+        <to-do-form />
         <ul>
             <li v-for="items in ToDoItems" :key="items.id">
                 <to-do-item :id="items.id" :label="items.label" :done="items.done" />
@@ -10,12 +11,14 @@
 </template>
 
 <script>
+import ToDoForm from "./components/ToDoForm.vue";
 import ToDoItem from "./components/ToDoItem.vue";
 import uniqueId from "lodash.uniqueid";
 
 export default {
     name: "App",
     components: {
+        ToDoForm,
         ToDoItem,
     },
     data() {
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,16 @@
+<template>
+    <form>
+        <label for="new-todo-input"> What needs to be done? </label>
+        <input type="text" id="new-todo-input" name="new-todo" autocomplete="off" />
+        <button type="submit">Add</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {},
+    data() {
+        return {};
+    },
+};
+</script>
