8efc680b503ac4511b39e4cb62cbcb23ee1cdbe5
idu-who <104561260+idu-who@users.noreply.github.com>
2024-04-16 16:44:31 +0400

    prod
    Signed-off-by: idu-who <104561260+idu-who@users.noreply.github.com>

--- src/App.vue
+++ src/App.vue
@@ -29,12 +29,7 @@ export default {
     },
     data() {
         return {
-            ToDoItems: [
-                { id: uniqueId("todo-"), label: "Learn Vue", done: false },
-                { id: uniqueId("todo-"), label: "Create a Vue project with the CLI", done: true },
-                { id: uniqueId("todo-"), label: "Have fun", done: true },
-                { id: uniqueId("todo-"), label: "Create a to-do list", done: false },
-            ],
+            ToDoItems: [{ id: uniqueId("todo-"), label: "Sample Todo", done: false }],
         };
     },
     methods: {
